13441d88375d8c7f3bd3466e21dd5f5d766a4291
santosh1206 <tukun1206@gmail.com>
2025-11-18 14:35:47 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,19 +1,13 @@
+# Configure the Azure Provider
 terraform {
-  required_version = ">=1.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
-skip_provider_registration = "true"
   features {}
 }
