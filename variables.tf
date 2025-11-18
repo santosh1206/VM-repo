3e6fecb1d61c06a63d893724b44ecb55864582ac
santosh1206 <tukun1206@gmail.com>
2025-11-18 14:36:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,20 @@
-variable "resource_group_location" {
-  default     = "eastus"
-  description = "Location of the resource group."
+# --- Variables (Recommended Practice) ---
+variable "resource_group_name" {
+  default = "vm-rg-custom-disk"
 }
-
-variable "prefix" {
-  type        = string
-  default     = "win-vm-iis"
-  description = "Prefix of the resource name"
+variable "location" {
+  default = "East US"
+}
+variable "vm_name" {
+  default = "custom-disk-linux-vm"
+}
+variable "disk_name" {
+  default = "my-custom-os-disk-tf" # Your desired OS Disk name
+}
+variable "admin_username" {
+  default = "azureuser"
+}
+# Note: You must ensure this file exists and contains your public SSH key
+variable "ssh_public_key_path" {
+  default = "~/.ssh/id_rsa.pub" 
 }
